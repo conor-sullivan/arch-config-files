4a98684cdb07349df64873fd859a8497e0aec091
conor-sullivan <130321871+conor-sullivan@users.noreply.github.com>
2023-09-07 14:55:15 -0400

    i3 and polybar configs

---
+++ polybar/launch.sh
@@ -0,0 +1,18 @@
+# Basic script to kill all old bars and launch new.
+
+# Terminate already running bad instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload example &
+  done
+else
+  polybar --reload example &
+fi
+# Launch the example bar
+
+
